30d7522d8d8c58c777cae46d62b2a4d33ffd131b
gryantgilgan <bryantgilgan@gmail.com>
2023-12-05 20:15:18 -0800

    combines create and create spartan function

--- controllers/api/spartans.js
+++ controllers/api/spartans.js
@@ -1,6 +1,71 @@
+const Spartan = require("../../models/spartan");
+const axios = require("axios");
 
+const create = async (req, res) => {
+  console.log(req.body.description);
+  try {
+    // Extract Spartan data from request body
+    const { user, name, hair, hairLength, eye, armor, armorColor, bio } =
+      req.body;
 
+    // Construct the prompt based on properties
+    const prompt = `Imagine a Spartan warrior inspired by the iconic characters from the Halo universe. This Spartan, stands tall in a lush forest. With ${hairLength} ${hair} hair flowing in the wind and ${eye} eyes scanning the surroundings with determination, the Spartan is clad in ${armor} armor with a ${armorColor} hue. Confidently holding their helmet under their left arm, the Spartan's ${hair} hair adds to the dynamic scene. The forested landscape provides a picturesque backdrop, with towering trees and vibrant greenery. Capture the essence of this powerful scene in an image, showcasing the Spartan's full body and the serenity of the forest.`;
 
+    // Prepare the data payload for the OpenAI API call
+    const data = JSON.stringify({
+      prompt: prompt,
+    });
+
+    // Configure the Axios request
+    const config = {
+      method: "post",
+      maxBodyLength: Infinity,
+      url: "https://api.openai.com/v1/images/generations",
+      headers: {
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        "Content-Type": "application/json",
+        // Add any additional headers if needed
+      },
+      data: data,
+    };
+
+    // Make the Axios request to OpenAI
+    const response = await axios.request(config);
+
+    // Extract the generated image data from the OpenAI response
+    const generatedImageData = response.data;
+    const imageUrl = generatedImageData.data[0].url;
+    console.log(generatedImageData.data[0].url);
+    // Send the generated image data as the response to the client
+    // res.json(generatedImageData);
+
+    // Create new Spartan with the OpenAI generated image URL
+    const newSpartan = new Spartan({
+      user,
+      name,
+      hair,
+      hairLength,
+      eye,
+      armor,
+      armorColor,
+      bio,
+      imageUrl,
+    });
+
+    // Save the New Spartan
+    const savedSpartan = await newSpartan.save();
+
+    // Send the saved Spartan as the response to the client
+    res.status(201).json(savedSpartan);
+  } catch (error) {
+    // Handle any errors that occur during the process
+    console.error("Error generating image:", error);
+    // Send an error response to the client
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+module.exports = { create };
 
 // async function createSpartan(req, res) {
 //   try {
@@ -28,50 +93,47 @@
 //   }
 // }
 
-const Spartan = require("../../models/spartan");
-const axios = require("axios");
+// const Spartan = require("../../models/spartan");
+// const axios = require("axios");
 
 
 
-const create = async (req, res) => {
+// const create = async (req, res) => {
 
-  console.log(req.body.description)
-  try {
-    // Prepare the data payload for the OpenAI API call
-    const data = JSON.stringify({
-      "prompt": req.body.description
-    });
+//   console.log(req.body.description)
+//   try {
+//     // Prepare the data payload for the OpenAI API call
+//     const data = JSON.stringify({
+//       "prompt": req.body.description
+//     });
 
-    // Configure the Axios request
-    const config = {
-      method: 'post',
-      maxBodyLength: Infinity,
-      url: 'https://api.openai.com/v1/images/generations',
-      headers: { 
-        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`, 
-        'Content-Type': 'application/json',
-        // Add any additional headers if needed
-      },
-      data: data,
-    };
+//     // Configure the Axios request
+//     const config = {
+//       method: 'post',
+//       maxBodyLength: Infinity,
+//       url: 'https://api.openai.com/v1/images/generations',
+//       headers: { 
+//         'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`, 
+//         'Content-Type': 'application/json',
+//         // Add any additional headers if needed
+//       },
+//       data: data,
+//     };
 
-    // Make the Axios request to OpenAI
-    const response = await axios.request(config);
+//     // Make the Axios request to OpenAI
+//     const response = await axios.request(config);
 
-    // Extract the generated image data from the OpenAI response
-    const generatedImageData = response.data;
-    console.log(generatedImageData.data[0].url)
-    // Send the generated image data as the response to the client
-    res.json(generatedImageData);
-  } catch (error) {
-    // Handle any errors that occur during the process
-    console.error("Error generating image:", error);
-    // Send an error response to the client
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-};
+//     // Extract the generated image data from the OpenAI response
+//     const generatedImageData = response.data;
+//     console.log(generatedImageData.data[0].url)
+//     // Send the generated image data as the response to the client
+//     res.json(generatedImageData);
+//   } catch (error) {
+//     // Handle any errors that occur during the process
+//     console.error("Error generating image:", error);
+//     // Send an error response to the client
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// };
 
-module.exports = { create };
-
-
-
+// module.exports = { create };
