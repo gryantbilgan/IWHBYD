a685d53a89822c18325efbe87d6a75c4f22e0d0f
gryantgilgan <bryantgilgan@gmail.com>
2023-12-06 14:18:05 -0800

    changes to more specific prompt

--- controllers/api/spartans.js
+++ controllers/api/spartans.js
@@ -9,7 +9,24 @@ const create = async (req, res) => {
       req.body.description;
 
     // Construct the prompt based on properties
-    const prompt = `Imagine a Spartan warrior inspired by the iconic characters from the Halo universe. This Spartan, stands tall in a lush forest. With ${hairLength} ${hair} hair flowing in the wind and ${eye} eyes scanning the surroundings with determination, the Spartan is clad in ${armor} armor with a ${armorColor} hue. Confidently holding their helmet under their left arm, the Spartan's ${hair} hair adds to the dynamic scene. The forested landscape provides a picturesque backdrop, with towering trees and vibrant greenery. Capture the essence of this powerful scene in an image, showcasing the Spartan's full body and the serenity of the forest.`;
+    const prompt = //`Imagine a Spartan warrior inspired by the iconic characters from the Halo universe. This Spartan, stands tall in a lush forest. With ${hairLength} ${hair} hair flowing in the wind and ${eye} eyes scanning the surroundings with determination, the Spartan is clad in ${armor} armor with a ${armorColor} hue. Confidently holding their helmet under their left arm, the Spartan's ${hair} hair adds to the dynamic scene. The forested landscape provides a picturesque backdrop, with towering trees and vibrant greenery. Capture the essence of this powerful scene in an image, showcasing the Spartan's full body and the serenity of the forest.`;
+
+    //`Generate an image of a Spartan warrior inspired by the iconic characters from the Halo universe. The Spartan stands tall in a vast desert landscape, with their face clearly visible. The Spartan has ${hair} hair of ${hairLength} length, flowing in the wind. Their eyes, colored ${eye}, scan the surroundings with determination.The Spartan is clad in ${armor} armor with a ${armorColor} hue. Confidently holding their helmet under their left arm, the Spartan's [choose hair color] hair adds to the dynamic scene. The surrounding desert provides a dramatic backdrop, with rolling dunes and a clear sky. Capture the essence of this powerful scene, showcasing the Spartan from head to toe.`
+
+    `Generate a vivid image of a Spartan warrior inspired by the iconic characters from the Bungie's Halo universe. In a desert landscape, depict the Spartan standing upright, facing forward, and holding their helmet securely under their left arm. Ensure the face of the Spartan is clearly visible with distinctive facial features.
+
+    For the Spartan's appearance:
+    - Hair Color: ${hair}
+    - Hair Length: ${hairLength}
+    - Eye Color: ${eye}
+
+    Regarding the armor:
+    - Armor Type: ${armor}
+    - Armor Color: ${armorColor}
+
+    Capture the essence of the Halo universe by emphasizing the unique and futuristic characteristics of the Spartan, ensuring a modern and recognizable interpretation. The focus should be on the Spartan's interaction with the desert environment and the distinct portrayal of their armor, facial features, and the act of holding the helmet.`
+
+
 
     // Prepare the data payload for the OpenAI API call
     const data = JSON.stringify({
