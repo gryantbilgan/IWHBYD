4bb381c7b7eb9e05f4d696d17e46dd1555eaa737
gryantgilgan <bryantgilgan@gmail.com>
2023-12-07 21:53:20 -0800

    adds style to spartan page

--- src/pages/SpartanPage/SpartanPage.jsx
+++ src/pages/SpartanPage/SpartanPage.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getAllImages } from "../../utilities/spartans-api";
 
-
 export default function SpartanPage() {
   const [generatedImages, setGeneratedImages] = useState([]);
 
@@ -12,20 +11,28 @@ export default function SpartanPage() {
   async function getImages() {
     try {
       const response = await getAllImages();
-      console.log(response)
+      console.log(response);
       setGeneratedImages(response);
     } catch (error) {
       console.log("Error Fetching Images:", error);
     }
   }
+
   return (
-    <div>
-      <h2 className="text-white">Fire Team DNC</h2>
+    <div className="flex flex-wrap min-h-1/2 text-white p-4">
+      <h2 className="text-lg font-bold mb-4 w-full text-center">Fire Team DNC</h2>
       {generatedImages.map((image) => (
-        <div key={image._id} className="text-white">
-          <img src={image.imageUrl} alt={`Spartan: ${image.name}`} />
-          <p>Name: {image.name}</p>
-          <p>Bio: {image.bio}</p>
+        <div
+          key={image._id}
+          className="bg-gray-800 p-2 rounded-md mb-4 max-w-xs w-1/5 mx-2 h-1/4"
+        >
+          <img
+            src={image.imageUrl}
+            alt={`Spartan: ${image.name}`}
+            className="mb-2 rounded-lg max-w-full h-1/4"
+          />
+          <p className="text-sm font-semibold">Name: {image.name}</p>
+          <p className="text-xs">Bio: {image.bio}</p>
         </div>
       ))}
     </div>
